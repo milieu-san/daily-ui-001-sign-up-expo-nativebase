fe5d4ffb1dcffdb22434db8003b4b414d407b532
miball0202 <aisacart@gmail.com>
2022-05-08 01:46:53 +0900

    Fix layout of ModalSignUpForm

--- src/components/ModalSignUpForm.tsx
+++ src/components/ModalSignUpForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormControl,
   Input,
@@ -21,19 +21,21 @@ interface Props {
 const ModalSignUpForm = (props: Props) => {
   const { isOpened, close } = props;
 
+  const [requesting, setRequesting] = useState<boolean>(false);
+
   return (
     <Slide in={isOpened} placement="bottom">
       <Box safeAreaTop />
       <Box flex={1} />
-      <KeyboardAvoidingView flex={2} bg="coolGray.900" borderTopRadius={25}>
+      <KeyboardAvoidingView flex={2} bg="coolGray.800" borderTopRadius={25}>
         <HStack h="40px" flexDirection="row-reverse">
           <CustomIconButton iconName="close" onPress={close} />
         </HStack>
-        <Center flex={3} mx={5}>
-          <VStack space={4} flex={1} w="100%">
-            <FormControl>
+        <Center flex={2} mx={7}>
+          <VStack space={5} w="100%">
+            <FormControl isDisabled={requesting}>
               <FormControl.Label>
-                <Text color="orange.500" fontWeight="bold">
+                <Text color="orange.500" fontWeight="bold" fontSize="md">
                   Email
                 </Text>
               </FormControl.Label>
@@ -41,11 +43,16 @@ const ModalSignUpForm = (props: Props) => {
                 borderColor="orange.500"
                 variant="rounded"
                 color="orange.500"
+                fontSize="md"
+                _focus={{
+                  borderColor: 'orange.300',
+                  backgroundColor: 'coolGray.700',
+                }}
               />
             </FormControl>
-            <FormControl>
+            <FormControl isDisabled={requesting}>
               <FormControl.Label color="orange.500">
-                <Text color="orange.500" fontWeight="bold">
+                <Text color="orange.500" fontWeight="bold" fontSize="md">
                   Password
                 </Text>
               </FormControl.Label>
@@ -54,12 +61,19 @@ const ModalSignUpForm = (props: Props) => {
                 borderColor="orange.500"
                 variant="rounded"
                 color="orange.500"
+                fontSize="md"
+                _focus={{
+                  borderColor: 'orange.300',
+                  backgroundColor: 'coolGray.700',
+                }}
               />
             </FormControl>
           </VStack>
         </Center>
-        <Box flex={1} mx={5}>
+        <Box flex={1} mx={7}>
           <Button
+            isLoading={requesting}
+            isLoadingText="Requesting..."
             variant="outline"
             colorScheme="orange"
             borderColor="orange.500"
@@ -68,6 +82,7 @@ const ModalSignUpForm = (props: Props) => {
               fontSize: 'md',
               fontWeight: 'bold',
             }}
+            onPress={() => setRequesting(true)}
           >
             Submit
           </Button>
