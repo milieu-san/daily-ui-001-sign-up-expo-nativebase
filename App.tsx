2ef4468a0c915a5e5c418143612dba7590295f09
miball0202 <aisacart@gmail.com>
2022-05-07 20:59:41 +0900

    Setup lint

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Center,
   Heading,
@@ -8,14 +8,14 @@ import {
   Box,
   Divider,
   Text,
-  Button
-} from "native-base";
+  Button,
+} from 'native-base';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const config = {
   dependencies: {
-    "linear-gradient": LinearGradient
-  }
+    'linear-gradient': LinearGradient,
+  },
 };
 
 const App = () => {
@@ -25,28 +25,35 @@ const App = () => {
       <Box flex={1} bg="darkBlue.900">
         <Box flex={1} justifyContent="center">
           <VStack space={5} alignItems="center">
-            <Heading size="lg" color="gray.100">Welcome to Pixel Gallery!!</Heading>
+            <Heading size="lg" color="gray.100">
+              Welcome to Pixel Gallery!!
+            </Heading>
           </VStack>
         </Box>
-        <Box flex={2} justifyContent="center" bg={{
-          linearGradient: {
-            colors: ["violet.800", "indigo.800"],
-            start: [0, 0],
-            end: [1, 0]
-          }
-        }} borderRadius={25} mx={3}>
+        <Box
+          flex={2}
+          justifyContent="center"
+          bg={{
+            linearGradient: {
+              colors: ['violet.800', 'indigo.800'],
+              start: [0, 0],
+              end: [1, 0],
+            },
+          }}
+          borderRadius={25}
+          mx={3}
+        >
           <VStack space={5} justifyContent="center" mx={3}>
-            <Text fontSize="md" color="gray.100" >Login with</Text>
-            <Button>
-
-            </Button>
+            <Text fontSize="md" color="gray.100">
+              Login with
+            </Text>
+            <Button />
           </VStack>
         </Box>
-        <Box flex={1}>
-        </Box>
+        <Box flex={1} />
       </Box>
     </NativeBaseProvider>
   );
-}
+};
 
-export default App
+export default App;
